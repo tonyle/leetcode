567a5d263a497aaa204bd436c12e4137b6344436
“Tony <Tonyle.microsoft@gmail.com>
2024-08-29 22:12:06 -0500

    searchInsert

---
+++ easy/searchInsert.ts
@@ -0,0 +1,18 @@
+function searchInsert(nums: number[], target: number): number {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return left;
+};
